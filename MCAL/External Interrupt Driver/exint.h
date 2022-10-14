4baada7654ba2aea92ed325b0d4673e6d9d154a4
shebl007byte <omar.mahmoud001@eng-st.cu.edu.eg>
2022-10-14 13:02:36 +0200

    upd: exint.h
    Finalized exint.h

--- MCAL/External Interrupt Driver/exint.h
+++ MCAL/External Interrupt Driver/exint.h
@@ -1,8 +1,15 @@
-#ifndef _EXINT_H_
+ #ifndef _EXINT_H_
 #define	_EXINT_H_
 #include "../registers.h"
 
-__asm__ __volatile__ ("sei" ::: "memory");
+
+# define sei()  __asm__ __volatile__ ("sei" ::: "memory")
+# define cli()  __asm__ __volatile__ ("cli" ::: "memory")
+
+#define EXT_INT_0 __vector_1
+
+#define ISR(INT_VECT) void INT_VECT(void) __attribute__ ((signal, used)); \
+void INT_VECT(void)
 
 typedef enum EN_interrupt_t {
     INT0, INT1
