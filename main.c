174dbf56e80854ac8ac3ad99f549595be1e4b496
shebl007byte <omar.mahmoud001@eng-st.cu.edu.eg>
2022-10-15 11:37:43 +0200

    mod: main.c
    Modified _main function to include only APP_init() & APP_main() which contain the logic code for the system.

--- main.c
+++ main.c
@@ -5,35 +5,11 @@
  * Created on October 8, 2022, 6:42 PM
  */
 
-#include "MCAL/External Interrupt Driver/exint.c"
-#include "MCAL/Timer Driver/timer.c"
-#include "ECUAL/LED Driver/led.c"
-#include "ECUAL/Button Driver/button.c"
+#include "Application/app.c"
 
-ST_pinPort_t CAR_RED_LED = {0, PORT_C};
-ST_pinPort_t CAR_YELLOW_LED = {1, PORT_C};
-ST_pinPort_t CAR_GREEN_LED = {2, PORT_C};
-
-ST_pinPort_t HUMAN_RED_LED = {0, PORT_B};
-ST_pinPort_t HUMAN_YELLOW_LED = {1, PORT_B};
-ST_pinPort_t HUMAN_GREEN_LED = {2, PORT_B};
-
-ST_pinPort_t HUMAN_BUTTON = {2, PORT_D };
-    
 int main(void) {
-    LED_init(CAR_RED_LED);
-    LED_init(CAR_YELLOW_LED);
-    LED_init(CAR_GREEN_LED);
-    LED_init(HUMAN_RED_LED);
-    LED_init(HUMAN_YELLOW_LED);
-    LED_init(HUMAN_GREEN_LED);
-
-    BUTTON_init(HUMAN_BUTTON);
-    
-    while (1) {
-        LED_tog(CAR_RED_LED);
-        TIMER0_second(5);
-    }
-    
+    APP_init();
+    APP_main();
     return 0;
 }
+
