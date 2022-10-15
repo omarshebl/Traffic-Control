1b39e2b08289fdc908606372960b26cd40f67db7
shebl007byte <omar.mahmoud001@eng-st.cu.edu.eg>
2022-10-15 11:36:31 +0200

    mod: app.c/app.h
    Moved source code from main.c to app.c

--- Application/app.c
+++ Application/app.c
@@ -1,1 +1,42 @@
 #include "app.h"
+#include "../MCAL/External Interrupt Driver/exint.c"
+#include "../MCAL/Timer Driver/timer.c"
+#include "../ECUAL/LED Driver/led.c"
+#include "../ECUAL/Button Driver/button.c"
+
+ST_pinPort_t CAR_RED_LED = {0, PORT_C};
+ST_pinPort_t CAR_YELLOW_LED = {1, PORT_C};
+ST_pinPort_t CAR_GREEN_LED = {2, PORT_C};
+ST_pinPort_t HUMAN_RED_LED = {0, PORT_B};
+ST_pinPort_t HUMAN_YELLOW_LED = {1, PORT_B};
+ST_pinPort_t HUMAN_GREEN_LED = {2, PORT_B};
+
+ST_pinPort_t HUMAN_BUTTON = {2, PORT_D };
+
+uint8_t ledstate = 0;
+
+void APP_init() {
+    LED_init(CAR_RED_LED);
+    LED_init(CAR_YELLOW_LED);
+    LED_init(CAR_GREEN_LED);
+    LED_init(HUMAN_RED_LED);
+    LED_init(HUMAN_YELLOW_LED);
+    LED_init(HUMAN_GREEN_LED);
+    
+    BUTTON_init(HUMAN_BUTTON);
+    
+    sei();
+    INT_init(INT0, RISE_EDGE);
+}
+
+void APP_main() {
+    
+    while (1) {
+        
+    }
+}
+
+ISR(EXT_INT_0) {
+    if(ledstate < 3) ledstate++;
+    else ledstate=0;
+}
--- Application/app.h
+++ Application/app.h
@@ -1,5 +1,8 @@
 #ifndef _APP_H_
 #define	_APP_H_
 
+void APP_init();
+void APP_main();
+
 #endif	/* APP_H */
 
